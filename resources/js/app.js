e6196baada0e1a947248660aaae6d2cf1078ac76
Yonas <yonas.ghebremedhin@gmail.com>
2024-04-13 12:41:44 +0200

    Connecting Code with shopify site

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
-alert("Hello world");
+document.addEventListener('DOMContentLoaded', function() {
+    const header = document.querySelector('.header');
+
+    const accountIcon = document.createElement('div');
+    accountIcon.classList.add('header__icon', 'header__icon--profile', 'link', 'focus-inset');
+
+    accountIcon.innerHTML = `
+    <img src="https://cdn.shopify.com/s/files/1/0694/3732/5533/files/user.png?v=1712920812" alt="">
+    `;
+
+    header.insertBefore(accountIcon, header.firstChild);
+
+    accountIcon.addEventListener('click', function() {
+        console.log('Clicked on account icon');
+    });
+});
